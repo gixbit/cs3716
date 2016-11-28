3753b1fd599db575a4847fac27f961884982af51
jaimB <jlb812@mun.ca>
2016-11-28 10:37:15 -0330

    Add files via upload

--- SingleElim.java
+++ SingleElim.java
@@ -1,44 +1,73 @@
 package skeletonCode;
 
 import java.awt.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class SingleElim extends JFrame{
-	private JLabel alert;
-	private JLabel alert1;
+public class SingleElim extends JPanel{
 	private JPanel panel;
-	private JPanel panel1;
-	private JPanel panel2;
-	
-	public SingleElim(){
-		alert = new JLabel("Team1");
-		alert.setFont(new Font("Arial", Font.PLAIN, 20));
-		alert1 = new JLabel("Team2");
-		alert1.setFont(new Font("Arial", Font.PLAIN, 20));
+	private int numOfTeams;
+	private int colnNum;
+	private int spacers;
+	private int side;
+	private int top;
+	private int bottom;
+	private ArrayList<JTextArea> panList = new ArrayList<JTextArea>();
 
+	public SingleElim(int n, int i){
+		super();
+		this.setBackground(Color.WHITE);
+		colnNum = i;
+		numOfTeams = n;
+		spacers = (int) Math.pow(2, colnNum)-1;
 		createPanel();
-		setSize(500,200);
-		setTitle("Alert");
 	}
-
-	public void createPanel(){
-		panel = new JPanel(new GridLayout(0,5));
-		panel1 = new JPanel();
-		panel2 = new JPanel();
-
+	
+	private void createPanel(){
+		MatteBorder line;
 		TitledBorder title;
-		Border blackline = BorderFactory.createLineBorder(Color.black);
-		title = BorderFactory.createTitledBorder(blackline, "title");
-		title.setTitleJustification(TitledBorder.RIGHT);
-		panel1.setBorder(title);
-		panel2.setBorder(title);
-		
-		panel1.add(alert);
-		panel2.add(alert1);
-		panel.add(panel1);
-		panel.add(panel2);
+		Border empty = BorderFactory.createEmptyBorder();
+		title = BorderFactory.createTitledBorder(empty, "title");
+		title.setTitlePosition(TitledBorder.RIGHT);
+
+		panel = new JPanel(new GridLayout(2*numOfTeams-1, 1,0,0));
+		//creates each column
+		int height = (int)Math.pow(2, colnNum+1);
+		for(int j = 0; j<2*numOfTeams-1; j++){
+			panList.add(new JTextArea(2,8));
+			if((j-spacers)%height == 0){
+				top = 1;
+				bottom = 0;
+				side = 5;
+			}
+			else if((j-spacers)%height == height-1){
+				top = 0;
+				bottom = 1;
+				side = 5;	
+			}
+			else{
+				top = 0;
+				bottom = 0;
+				side = 5;
+			}
+			line = BorderFactory.createMatteBorder(top, 0, bottom, side, Color.black);
+			panList.get(j).setBorder(line);
+			if(j < spacers || j > 2*numOfTeams-spacers-2){
+				line = BorderFactory.createMatteBorder(0, 0, 0, 0, Color.black);
+				panList.get(j).setBorder(line);
+			}
+			panList.get(j).setEditable(false);
+			panel.add(panList.get(j));
+		}
+/*		for(int k){
+			for(int i = spacers; i < height+spacers; i++){
+				line = BorderFactory.createMatteBorder(0, 0, 0, 0, Color.black);
+				panList.get(i).setBorder(line);
+			}
+*/		line = BorderFactory.createMatteBorder(1, 0, 0, 0, Color.black);
+		panList.get(2*numOfTeams-spacers-2).setBorder(line);
 		add(panel);
 	}
-
 }
---
+++ doubleElim.java
@@ -0,0 +1,14 @@
+/*
+public class doubleElim implements TournamentStructure{
+	String structureName; 
+	
+	public String getStructureName() {
+		return structureName;
+	} 
+	
+	public void createBrackets() {
+	// This will create brackets and be in line with this tournament structure. 
+	}
+	
+}
+*/
