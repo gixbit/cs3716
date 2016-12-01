5977485a773de37158fdafa2a5b2b356ebd38517
pitoucc <pitoucc@hotmail.com>
2016-12-01 11:04:27 -0330

    added file loader to viewer

--- cs3716/src/GUI/Viewer.java
+++ cs3716/src/GUI/Viewer.java
@@ -1,6 +1,14 @@
 package GUI;
 
 import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /**
  * This is the entry point for the Tournament GUI
@@ -13,9 +21,37 @@ import javax.swing.JFrame;
  * @author Karl Chiasson
  */
 public class Viewer {	
+
+	public static ArrayList<Tournament> Tournaments;
+
+
 	public static void main(String[] args){
 		JFrame frame = new MainScreen();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter(){
+			public void windowOpened(WindowEvent e) {
+        		try {
+        			FileInputStream fis = new FileInputStream("Tournaments.txt");
+        			ObjectInputStream ois = new ObjectInputStream(fis);
+        			try {
+        				Tournaments = (ArrayList<Tournament>)ois.readObject();
+        				if(Tournaments == null) {
+        					Tournaments = new ArrayList<Tournament>();
+        				}
+        			} catch(IOException | ClassNotFoundException err) {
+        				Tournaments = new ArrayList<Tournament>();
+        			} finally {
+        				ois.close();
+        				fis.close();	
+        			}
+        		} catch (FileNotFoundException err) {
+    				Tournaments = new ArrayList<Tournament>();
+        		} catch (IOException err) {
+        			//Could not close
+        		}
+            }
+        });
+
 	}
 }
