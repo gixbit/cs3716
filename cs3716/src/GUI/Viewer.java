2dfac53bbd80df98f09fd203cbba6c78e35c68a3
John Hollett <g1xb17@gmail.com>
2016-12-01 12:20:28 -0330

    Random tests

--- cs3716/src/GUI/Viewer.java
+++ cs3716/src/GUI/Viewer.java
@@ -29,7 +29,6 @@ public class Viewer {
 	public static ArrayList<Tournament> Tournaments;
 
 	public static void main(String[] args){
-		System.out.println("test");
 		JFrame frame = new MainScreen();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
